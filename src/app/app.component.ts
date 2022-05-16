23bfcdfdaa4b707a29d6607e26474dfff9aeb2df
João Leonardo Pereira <joaoleonardo_pereira@apple.com>
2022-05-16 10:55:48 -0300

    pwa stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { SwUpdate } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private updates: SwUpdate) {
+    this.updates.versionUpdates.subscribe(event => {
+      this.updates.activateUpdate().then(() => {
+        document.location.reload()
+      })
+    })
+  }
 }
